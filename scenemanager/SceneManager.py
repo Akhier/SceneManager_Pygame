5755c990cb1300d3ae1e936545daa870aebc31cd
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2017-07-29 22:46:52 -0400

    Finish SceneManager class

--- scenemanager/SceneManager.py
+++ scenemanager/SceneManager.py
@@ -13,4 +13,16 @@ class SceneManager:
         self.screen = pygame.display.set_mode((resolution))
         pygame.display.set_caption(settings['caption'])
         self.clock = pygame.time.Clock()
+        self.fps = settings['fps']
         self.quit_flag = False
+
+    def loop(self):
+        while not self.quit_flag:
+            self.clock.tick(self.fps)
+            self.scene.on_event(pygame.event)
+            self.scene.on_update()
+            self.scene.on_render(self.screen)
+            pygame.display.flip()
+
+    def change_scene(self, scene):
+        self.scene = scene
