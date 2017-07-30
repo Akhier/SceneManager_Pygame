296863ac5133bbd141d0ccbab5a4aaca0a1a1bc6
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2017-07-29 22:38:21 -0400

    Finish __init__ for SceneManager class

--- scenemanager/SceneManager.py
+++ scenemanager/SceneManager.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 
 
 class SceneManager:
@@ -6,4 +7,10 @@ class SceneManager:
     """
 
     def __init__(self):
-        self.screen = pygame.display.set_mode(())
+        with open("SceneManagerSettings.json") as json_data:
+            settings = json.load(json_data)
+        resolution = (settings['screen_width'], settings['screen_height'])
+        self.screen = pygame.display.set_mode((resolution))
+        pygame.display.set_caption(settings['caption'])
+        self.clock = pygame.time.Clock()
+        self.quit_flag = False
