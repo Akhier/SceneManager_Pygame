94635312862191709478274254b6baa896c40957
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2017-07-29 22:49:14 -0400

    Finish __init__ for scenemanager

--- scenemanager/__init__.py
+++ scenemanager/__init__.py
@@ -1,4 +1,7 @@
 __version__ = "0.1.0"
 __author__ = "Akhier Dragonheart"
 __license__ = "MIT"
-__all__ = ""
+__all__ = "SceneManager, SceneTemplate"
+
+from SceneManager import SceneManager
+from SceneTemplate import SceneTemplate
